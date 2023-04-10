2306e4b605816f8443b29bca1aa3dc1ce5aff3e4
Jaison John <jaison.john@whiterabbit.group>
2023-04-10 09:14:45 +0530

    layout initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainLayout from "layouts/MainLayout";
+
 function App() {
-  return <div className="w-screen h-screen"></div>;
+  return (
+    <Router>
+      <div className="w-screen h-screen">
+        <Routes>
+          <Route element={MainLayout}>
+
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
