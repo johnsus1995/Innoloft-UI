415f143e5a5a61da9340f5d3927a6b04e15a785e
Jaison John <jaison.john@whiterabbit.group>
2023-04-16 11:25:37 +0530

    navbar fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,33 +1,30 @@
 import PropTypes from "prop-types";
 import { ReactComponent as Logo } from "src/assets/icons/logo.svg";
 import { AiOutlineMessage } from "react-icons/ai";
-import { CiGlobe } from "react-icons/ci";
 import { SlBell } from "react-icons/sl";
 import { BsChevronDown } from "react-icons/bs";
-
-import ProfilePic from "src/assets/images/profilePic.png";
 
 const Navbar = (props) => {
   return (
     <div className="Navbar">
-      <div className="w-full h-[55px] px-[240px] bg-secondary_white flex items-center justify-between">
-        <div className="w-[140px] h-[26.29px]">
+      <div className="  flex justify-between items-center bg-secondary_white h-[55px]  mx-auto px-4">
+        <div className="min-w-[140px] min-h-[26.29px] ml-0 ">
           <Logo />
         </div>
         <div className="border">
           <input
-            className="w-[500px] h-[27px] p-2 rounded-md"
+            className="hidden md:flex md:w-[300px] lg:w-[350px] xl:w-[500px]  h-[27px] p-2 rounded-md "
             placeholder="Enter interests, keyword, company name, etc."
             type="text"
           />
         </div>
         <div>
-          <ul className="flex gap-5 justify-between items-center">
+          <ul className=" hidden lg:flex gap-5 justify-between items-center md:flex">
             <li>
               <AiOutlineMessage />
             </li>
-            <li className="flex gap-1">
-              <CiGlobe />
+            <li className="flex gap-1 items-center">
+              <p>EN</p>
               <BsChevronDown />
             </li>
             <li>
