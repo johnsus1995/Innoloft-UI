85683079426942463fbdd05a46dcdec20d823f44
Jaison John <thejohnjaison@yahoo.com>
2023-04-23 23:29:52 +0530

    apis added

--- src/api/product.js
+++ src/api/product.js
@@ -4,16 +4,34 @@ import customAxios from "../utils/api";
 export const get = (config={}) => {
     // const {} = config
     return customAxios({
-      url: "/users/signin",
-      method: "POST",
+      url: "/product/6781/",
+      method: "GET",
       ...config
     });
   };
   
   export const update = (data, options) => {
     return customAxios({
-      url: "/users/signup",
-      method: "POST",
+      url: "/product/6781/",
+      method: "PUT",
+      data,
+      ...options,
+    });
+  };
+
+  export const getTRL = (data, options) => {
+    return customAxios({
+      url: "/trl/",
+      method: "GET",
+      data,
+      ...options,
+    });
+  };
+
+  export const getAppConfig = (data, options) => {
+    return customAxios({
+      url: "/trl/",
+      method: "GET",
       data,
       ...options,
     });
